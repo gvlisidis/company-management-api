cacd4220675e08cd1b3d2085ef279bf1025d38e5
George Vlisidis <gblisidis@gmail.com>
2021-09-02 16:37:40 +0100

    Set up Role model and relationship with user.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        \App\Models\User::factory(1)->create(
+
+        Role::create(['name' => 'Admin']);
+        Role::create(['name' => 'User']);
+
+        User::factory()->create(
             [
                 'name' => 'George',
                 'email' => 'gv@mail.com',
+                'role_id' => 1,
                 'email_verified_at' => null,
             ]
         );
